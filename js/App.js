69928444b5a0f4eb32c16c2f92c4148d267a90a7
Steve Choi <stevechoi93@gmail.com>
2019-02-25 15:22:22 -0800

    first commit

---
+++ js/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: "white", paddingTop: 50 }}>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
