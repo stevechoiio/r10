9bec437893d784ad78b0f2f16676fa89e43dd7a9
Steve Choi <stevechoi93@gmail.com>
2019-02-26 11:08:16 -0800

    added assests including fonts

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -8,7 +8,7 @@ import Favourites from "../screens/Favourites";
 import Maps from "../screens/Maps";
 import Schedule from "../screens/Schedule";
 import Session from "../screens/Session";
-
+import Icon from "react-native-vector-icons/Ionicons";
 const AboutStack = createStackNavigator({
   About
 });
@@ -32,6 +32,27 @@ export default createBottomTabNavigator(
     About: AboutStack
   },
   {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+
+        let iconName;
+        if (routeName === "About") {
+          iconName = `ios-information-circle`;
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+        } else if (routeName === "Schedule") {
+          iconName = `ios-options`;
+        } else if (routeName === "Maps") {
+          iconName = "ios-map";
+        } else if (routeName === "Faves") {
+          iconName = `ios-heart`;
+        }
+
+        // You can return any component that you like here!
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      }
+    }),
     tabBarOptions: {
       activeTintColor: "#fff",
       inactiveTintColor: "#999",
