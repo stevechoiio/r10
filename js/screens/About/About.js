7ff0c9e313a994cf4bc8eadcb5e08e792aa85556
Steve Choi <stevechoi93@gmail.com>
2019-03-01 10:58:42 -0800

    added animation

--- js/screens/About/About.js
+++ js/screens/About/About.js
@@ -1,18 +1,64 @@
 import React, { Component } from "react";
-import { Text, View, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  LayoutAnimation,
+  TouchableOpacity
+} from "react-native";
+
+class Collapsable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      closed: true
+    };
+  }
+  onPress = () => {
+    LayoutAnimation.easeInEaseOut();
+    this.setState({ closed: !this.state.closed });
+  };
+  // shouldComcoponentUpdate() {
+  //   this.setState({ closed: true });
+  // }
+  render() {
+    return (
+      <View>
+        <TouchableOpacity onPress={this.onPress}>
+          <View>
+            <Text style={{ fontSize: 20 }}>
+              {!this.state.closed ? "-" : "+"}
+              {this.props.item.title}
+            </Text>
+            {!this.state.closed ? (
+              <Text>
+                {this.props.item.description} {this.props.item.order}
+              </Text>
+            ) : null}
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
 export default class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opened: []
+    };
+  }
   render() {
     return (
       <View style={{ backgroundColor: "white" }}>
+        <Text>asdf</Text>
         <FlatList
           style={{ color: "black" }}
           data={this.props.data.allConducts}
           renderItem={({ item }) => (
             <View>
-              <Text style={{ fontSize: 50 }}>{item.title}</Text>
-              <Text>
-                {item.description} {item.order}
-              </Text>
+              <Collapsable item={item} />
             </View>
           )}
           keyExtractor={(item, index) => item.id}
